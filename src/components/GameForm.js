808db4b2b2aa10df49a706be4a31317943160bd9
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2018-02-09 18:34:56 +1100

    dealing with types forms

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -4,7 +4,10 @@ import PropTypes from "prop-types";
 class GameForm extends Component {
   state = {
     name: "",
-    description: ""
+    description: "",
+    price: 0,
+    duration: 0,
+    players: ""
   };
 
   handleSubmit = e => {
@@ -12,7 +15,17 @@ class GameForm extends Component {
     console.log(this.state);
   };
 
-  handelChange = e => this.setState({ [e.target.name]: e.target.value });
+  // handelChange = e =>
+  //   this.setState({
+  //     [e.target.name]:
+  //       e.target.type === "number"
+  //         ? parseInt(e.target.value, 10)
+  //         : e.target.value
+  //   });
+
+  handelStringChange = e => this.setState({ [e.target.name]: e.target.value });
+  handelNumberChange = e =>
+    this.setState({ [e.target.name]: parseInt(e.target.value, 10) });
 
   // handleNameChange = e => this.setState({ name: e.target.value });
   // handleDescriptionChange = e => this.setState({ description: e.target.value });
@@ -28,7 +41,7 @@ class GameForm extends Component {
             name="name"
             placeholder="Full game title"
             value={this.state.name}
-            onChange={this.handelChange}
+            onChange={this.handelStringChange}
           />
         </div>
 
@@ -40,8 +53,40 @@ class GameForm extends Component {
             name="description"
             placeholder="Full game title"
             value={this.state.description}
-            onChange={this.handelChange}
+            onChange={this.handelStringChange}
           />
+        </div>
+        <div className="three fields">
+          <div className="field">
+            <label htmlFor="price" /> Price (in cents)
+            <input
+              type="number"
+              id="price"
+              name="price"
+              value={this.state.price}
+              onChange={this.handelNumberChange}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="duration" /> Duration (in min)
+            <input
+              type="number"
+              id="duration"
+              name="duration"
+              value={this.state.duration}
+              onChange={this.handelNumberChange}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="players" /> Players
+            <input
+              type="text"
+              id="players"
+              name="players"
+              value={this.state.players}
+              onChange={this.handelStringChange}
+            />
+          </div>
         </div>
         <button className="ui button" type="submit">
           Submit
