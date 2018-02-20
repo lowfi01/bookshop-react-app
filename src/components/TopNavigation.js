2cc78a2ccedea835dc2e463c07246f126a77c6ed
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2018-02-20 19:31:28 +1100

    Use NavLink Refactor

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const TopNavigation = ({ showGameForm, showPublisher }) => (
   <div className="ui secondary pointing menu">
-    <Link to="/" className="item">
+    <NavLink exact to="/" className="item">
       BGShop
-    </Link>
-    <Link to="/games " className="item">
+    </NavLink>
+    <NavLink exact to="/games " className="item">
       Games
-    </Link>
-    <Link to="/games/new " className="item">
+    </NavLink>
+    <NavLink exact to="/games/new " className="item">
       <i className="icon plus" /> Add New Game
-    </Link>
+    </NavLink>
   </div>
 );
 
